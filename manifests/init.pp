a69fd3c4cadf943cd1ec5fb730d4380ea593f8a0
Sridhar Basam <sridhar.basam@gmail.com>
2013-01-15 09:33:29 -0500

    Added dashboard and report support into puppet module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,98 +1,115 @@
-class puppet($run_master = false,
-             $run_agent = false,
-             $puppetmaster_address = "",
-             $certname = "",
-             $master_autosign_cert = undef,
-             $runinterval = 120,
-             $extra_modules = "",
-             $mysql_root_password = 'changeMe',
-             $mysql_password = 'changeMe') {
+class puppet(
+  $run_master = false,
+  $run_agent = false,
+  $puppetmaster_address = "",
+  $certname = "",
+  $master_autosign_cert = undef,
+  $runinterval = 120,
+  $report_url = "http://127.0.0.1:8080/reports/upload",
+  $reports,
+  $report_enable = true,
+  $extra_modules = "",
+  $mysql_root_password = 'changeme',
+  $mysql_password = 'changeme',
+  $dashboard = true
+) {
 
-	package { puppet-common:
-		ensure => present
-	}
+  package { puppet-common:
+    ensure => present
+  }
+# install latest facter version due to issues with ubuntu one
+  package { 'facter':
+    ensure => latest
+  }
+  if ($run_master) {
+# run master under passenger
+    package { "puppetmaster-passenger":
+      ensure => present
+    }
 
-	if ($run_master) {
-		package { "puppetmaster-passenger":
-			ensure => present
-		}
+    apache::vhost { 'puppetmaster':
+      priority           => '20',
+      port               => '8140',
+      docroot            => '/usr/share/puppet/rack/puppetmasterd/public/',
+      template           => 'puppet/puppetmaster-vhost.erb',
+      configure_firewall => false,
+    }
 
-		# set up mysql server
-		class { 'mysql::server':
-			config_hash => {
-				'root_password' => $mysql_root_password,
-				'bind_address'  => '127.0.0.1'
-			}
-		}
+# set up mysql server
+    class { 'mysql::server':
+      config_hash => {
+        'root_password' => $mysql_root_password,
+        'bind_address'  => '127.0.0.1'
+      }
+    }
 
-		mysql::db { puppet:
-			user         => puppet,
-			password     => $mysql_password,
-			host         => localhost,
-		}
+    mysql::db { 'puppet':
+      user         => puppet,
+      password     => $mysql_password,
+      host         => localhost,
+    }
+    package { 'ruby-activerecord':
+      ensure => present
+    }
 
-        file { "/var/lib/puppet/reports":
-            ensure => "directory",
-            owner => "puppet",
-            group => "puppet"
-        }
+    package { 'ruby-mysql':
+      ensure => present
+    }
 
-		package { "ruby-activerecord":
-			ensure => present
-		}
+    File <| title == '/etc/puppet/puppet.conf' |> {
+      notify +> Exec["restart-puppetmaster"]
+    }
 
-		package { "ruby-mysql":
-			ensure => present
-		}
+    file { '/etc/puppet/autosign.conf':
+      ensure  => present,
+      content => template('puppet/autosign.conf.erb'),
+    }
 
-		File <| title == "/etc/puppet/puppet.conf" |> {
-			notify +> Exec["restart-puppetmaster"]
-		}
+    exec { 'restart-puppetmaster':
+      command     => "/usr/sbin/service apache2 restart",
+      require     => Package["puppetmaster-passenger"],
+      refreshonly => true
+    }
 
-		file { "/etc/puppet/autosign.conf":
-			ensure => present,
-			content   => template('puppet/autosign.conf.erb'),
-		}
+# set up reports and dashboard under passenger
+    if ( $dashboard ) {
+      include puppet::dashboard
+    }
 
-		exec { "restart-puppetmaster":
-			command => "/usr/sbin/service apache2 restart",
-			require => Package["puppetmaster-passenger"],
-			refreshonly => true
-		}
-	}
+  }
 
-	if ($run_agent) {
-		package { puppet:
-			ensure => present
-		}
+  if ($run_agent) {
+    package { puppet:
+      ensure => present
+    }
 
-		file { "/etc/default/puppet":
-			content => template('puppet/defaults.erb'),
-			notify => Exec["restart-puppet"],
-		}
+    file { '/etc/default/puppet':
+      content => template('puppet/defaults.erb'),
+      notify  => Exec["restart-puppet"],
+    }
 
-		File <| title == "/etc/puppet/puppet.conf" |> {
-			notify +> Exec["restart-puppet"]
-		}
+    File <| title == '/etc/puppet/puppet.conf' |> {
+      notify +> Exec["restart-puppet"]
+    }
 
-		file { "/etc/init.d/puppet":
-			mode => 0755,
-			owner => root,
-			group => root,
-			content => template('puppet/init.erb'),
-			notify => Exec["restart-puppet"]
-		}
+    file { '/etc/init.d/puppet':
+      mode    => 0755,
+      owner   => root,
+      group   => root,
+      content => template('puppet/init.erb'),
+      notify  => Exec["restart-puppet"]
+    }
 
-		exec { "restart-puppet":
-			command => "/usr/sbin/service puppet restart",
-			require => Package[puppet],
-			refreshonly => true
-		}
+    exec { 'restart-puppet':
+      command => "/usr/sbin/service puppet restart",
+      require => Package[puppet],
+      refreshonly => true
+    }
 
-	}
+  }
 
-	file { "/etc/puppet/puppet.conf":
-		content => template('puppet/puppet.conf.erb'),
-		require => Package[puppet-common]
-	}
+  file { '/etc/puppet/puppet.conf':
+    content => template('puppet/puppet.conf.erb'),
+    require => Package[puppet-common]
+  }
 }
